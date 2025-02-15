843bbb16538deed0aa12e45b7db7067d83d72a5b
ved-b <90875244+ved-b@users.noreply.github.com>
2025-02-15 01:52:53 -0800

    Ball moves with player

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -7,18 +7,24 @@ public class BallController : MonoBehaviour
     [SerializeField] private InputManager inputManager;
     private bool isBallLaunched;
     private Rigidbody ballRB;
+
+    [SerializeField] private Transform ballAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
-        
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall(){
         if(isBallLaunched) return;
         isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.isKinematic = false;
+        transform.parent = null;
     }
 
     // Update is called once per frame

0ce0e58faa1dfc1f720a59dff813fb55947be5a8
ved-b <90875244+ved-b@users.noreply.github.com>
2025-02-14 22:36:32 -0800

    Fixed gutter bug

--- Assets/Gutter.cs
+++ Assets/Gutter.cs
@@ -2,25 +2,23 @@ using UnityEngine;
 
 public class Gutter : MonoBehaviour
 {
-
-    private void OnTriggerEnter(Collider triggeredBody){
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        // we first get the rigidbody of the ball
+        // and store it in a local variable ballRigidBody
         Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
-
-        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
-
-        ballRigidBody.linearVelocity = Vector3.zero;
-
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // Testing if this is the right branch being tracked
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        //We store the velocity magnitude before resetting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+        
+        //It is important to reset the linear AND angular velocity
+        //This is because the ball is rotating on the ground when moving
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+        
+        //Now we add force in the forward direction of the gutter
+        //We use the cached velocity magnitude to keep it a little realistic
+        ballRigidBody.AddForce(transform.forward * velocityMagnitude,
+        ForceMode.VelocityChange);
     }
 }
